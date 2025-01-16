d1be9d3ec6e9bdbd345851c86534f1ed6b7ad769
Robalcor <roberto.aldaz58@gmail.com>
2025-01-15 20:13:46 -0800

    agregue una tabla

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if 'experiment_no' not in st.session_state:
 
 if 'df_experiment_results' not in st.session_state:
     st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
-    
+
 st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
@@ -35,4 +35,15 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
+    st.session_state['df_esperiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                    columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiments_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
+st.write(st.session_state['df_experiment_results'])
